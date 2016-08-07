ac0025453e0875fc8066d43e664b29a780684bb3
shakilakarthi <shakilakarthi96@gmail.com>
2016-08-06 21:24:03 -0800

    Add files via upload

---
+++ Aaa.java
@@ -0,0 +1,11 @@
+
+public class Aaa {
+
+	
+	public static void main(String[] args) {
+		String name="what do you know about me";  
+		System.out.println(name.contains("do you know"));  
+        System.out.println(name.contains("shaki"));
+	}
+
+}
---
+++ Str.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Str {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the string :");
+		Scanner in =new Scanner(System.in);		
+        String s=in.nextLine();
+        int n=s.length();
+        System.out.println(n);
+
+	}
+
+}
